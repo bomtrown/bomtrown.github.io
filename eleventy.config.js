daa022fd765eb3a42bbb28dbecf742b97a0c10ab
bomtrown <tomwbrown222@gmail.com>
2026-07-28 13:51:47 +0100

    Ptycho Article, format consistency  update, footer update, garden update

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,6 +11,9 @@ module.exports = function(eleventyConfig) {
   // Tell Eleventy to copy any JSON files inside your projects folder!
   eleventyConfig.addPassthroughCopy("src/projects/**/*.json");
 
+  // Tell Eleventy to copy all image types from anywhere inside the projects folder
+  eleventyConfig.addPassthroughCopy("src/projects/**/*.{jpg,jpeg,png,gif,svg,webp}");
+
   // Tell Eleventy where your working files are, and where to output the site
   return {
     dir: {
--- src/projects/antptycho.md
+++
@@ -1,8 +0,0 @@
----
-title: The Future of Microscopes
-date: 2026-06-24
-tags: project
-image: media/images/ptychography.jpg
-description: Article soon, My award winning physics degree project. Click to explore the software I developed for this project.
-external_url: https://github.com/bomtrown/ANTPtycho
----
